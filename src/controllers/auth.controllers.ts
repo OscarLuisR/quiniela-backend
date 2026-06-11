e61c2f086544620803f4030438fca533b29063ce
Oscar Rodriguez <oscar.luis.rodriguez.r@gmail.com>
2026-06-11 14:27:08 -0400

    fix: cambio de cookie sameSite a lax para iOS

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -16,7 +16,6 @@ export async function loginController(
         httpOnly: true,
         secure: isProd,
         // sameSite: "none", // TODO: Se coloco None porque el frontend y el backend estan en dominio separados //isProd ? "strict" : "lax",
-        // sameSite: isProd ? "strict" : "lax",
         sameSite: "lax",
         maxAge: 1000 * 60 * 60,
     })
